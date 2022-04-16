3b127512938a9286ff7fdc4adf474d2f9181fac3
Michael Jakobsen <michael@rpair.co.uk>
2022-04-16 14:10:44 +0100

    added models and migrated the model

--- surfspots/admin.py
+++ surfspots/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from surfspots.models import Surfspot
+
 # Register your models here.
+admin.site.register(Surfspot)
--- surfspots/models.py
+++ surfspots/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Surfspot(models.Model):
+  """  SURFSPOT MODEL """
+  
+  name = models.CharField(max_length=100)
+  country = models.CharField(max_length=100)
+  address = models.CharField(max_length=100)
+  postcode = models.CharField(max_length=100)
+  created = models.DateTimeField(auto_now_add=True)
