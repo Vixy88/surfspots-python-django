eb3d048d9f742fbb856f36783dc4c9000730af9d
Michael Jakobsen <michael@rpair.co.uk>
2022-04-19 18:53:16 +0100

    added single object view to url routes in surfspots folder

--- surfspots/urls.py
+++ surfspots/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from surfspots.views import ShowListView
+from surfspots.views import ShowListView, ShowDetailView
 from . import views
 
 urlpatterns = [
-  path('', ShowListView.as_view())
+  path('', ShowListView.as_view()),
+  path('<str:pk>/', ShowDetailView.as_view())
 ]
--- surfspots/views.py
+++ surfspots/views.py
@@ -8,7 +8,14 @@ class ShowListView(views.APIView):
   def get(self, _request):
     # Gives me all the objects in the database as python objects
     # these python objects are instances of the Porkdishes class
-    recipes = Surfspot.objects.all()
-    serialized_surfspots = SurfspotSerializer(recipes, many=True)
+    surfspots = Surfspot.objects.all()
+    serialized_surfspots = SurfspotSerializer(surfspots, many=True)
     return response.Response(serialized_surfspots.data)
+
+
+class ShowDetailView(views.APIView):
+  def get(self, _request, pk):
+    surfspot = Surfspot.objects.get(id=pk)
+    serialized_surfspot = SurfspotSerializer(surfspot, many=False)
+    return response.Response(serialized_surfspot.data)
 
