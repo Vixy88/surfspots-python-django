af5d8cf997d84b1967afacd3df964da227d68ac3
Michael Jakobsen <michael@rpair.co.uk>
2022-05-05 22:51:04 +0100

    Updated the surfspots model to include latitude and longitude this will enable us to create a function that automatically populates them based on their address

--- backend/settings.py
+++ backend/settings.py
@@ -100,12 +100,6 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'surfspots',
-    #     'HOST': 'localhost',
-    #     'PORT': 5432,
-    # }
     'default': dj_database_url.config(
         default='postgres://michaeljakobsen@localhost/surfspots'
     )
--- surfspots/models.py
+++ surfspots/models.py
@@ -9,9 +9,11 @@ class Surfspot(models.Model):
   
   name = models.CharField(max_length=100, default=None)
   country = models.CharField(max_length=100, default=None)
-  city = models.CharField(max_length=100, null=True, blank=True)
   address = models.CharField(max_length=100, default=None)
+  city = models.CharField(max_length=100, null=True, blank=True)
   postcode = models.CharField(max_length=100, default=None)
+  latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, default='0')
+  longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, default='0')
   image = models.CharField(max_length=250, default=None)
   magic_seaweed_link = models.CharField(max_length=250, default=None)
   google_directions_link = models.CharField(max_length=300, default=None, null=True, blank=True)
